9c34974f068e1ea641d2d105008d654cf6ea0698
is386 <20195347+is386@users.noreply.github.com>
2026-08-10 22:30:57 -0400

    Add diagonal hazards

--- src/gameplay/hazard/hazard.gd
+++ src/gameplay/hazard/hazard.gd
@@ -31,12 +31,19 @@ func _build_hazard() -> void:
 			block.global_position.x = block_position
 		elif type == HazardType.VERTICAL:
 			block.global_position.y = block_position
+		elif type == HazardType.DIAGONAL_UP:
+			block.global_position.x = block_position
+			block.global_position.y = -block_position
+		elif type == HazardType.DIAGONAL_DOWN:
+			block.global_position.x = block_position
+			block.global_position.y = block_position
 
 		blocks.add_child(block)
 
 	var rectangle := RectangleShape2D.new()
 	var hazard_offset := (length - 1) * block_size / 2
 	var hazard_size := block_size * length
+	var diagonal_block_size := sqrt(pow(block_size, 2) * 2)
 
 	if type == HazardType.HORIZONTAL:
 		global_position.x -= hazard_offset
@@ -46,6 +53,20 @@ func _build_hazard() -> void:
 		global_position.y -= hazard_offset
 		collision_shape.position.y += hazard_offset
 		rectangle.size = Vector2(block_size, hazard_size)
+	elif type == HazardType.DIAGONAL_UP:
+		global_position.x -= hazard_offset
+		global_position.y += hazard_offset
+		collision_shape.position.x += hazard_offset
+		collision_shape.position.y -= hazard_offset
+		collision_shape.rotation_degrees = -45
+		rectangle.size = Vector2(diagonal_block_size * length, block_size)
+	elif type == HazardType.DIAGONAL_DOWN:
+		global_position.x -= hazard_offset
+		global_position.y -= hazard_offset
+		collision_shape.position.x += hazard_offset
+		collision_shape.position.y += hazard_offset
+		collision_shape.rotation_degrees = 45
+		rectangle.size = Vector2(diagonal_block_size * length, block_size)
 
 	collision_shape.shape = rectangle
 
