678b86a0bae6dc813143d23333d42ab53c61d48c
is386 <20195347+is386@users.noreply.github.com>
2026-08-10 21:21:20 -0400

    Add player flying

--- src/gameplay/player/player.gd
+++ src/gameplay/player/player.gd
@@ -1,5 +1,21 @@
 class_name Player
-extends Node2D
+extends CharacterBody2D
+
+@export var flying_speed: float = 200
+@export var fall_speed: float = 300
+
+
+func _physics_process(delta: float) -> void:
+	var vertical_speed := 0.0
+
+	if Input.is_action_pressed("fly"):
+		vertical_speed = -flying_speed
+	elif not is_on_floor():
+		vertical_speed = fall_speed
+
+	velocity.y = move_toward(velocity.y, vertical_speed, get_gravity().y * delta)
+	move_and_slide()
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
