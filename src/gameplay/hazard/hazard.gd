eea4cf46fdc42ecde9f44b0a1c6a8da36445d2ab
is386 <20195347+is386@users.noreply.github.com>
2026-08-10 23:11:58 -0400

    Refactor the hazard building algorithm
    The previous algorithm would change the position of the hazard after it
    was built. This led to an issue when implementing rotating hazards
    because the pivot point would be still be the origin. So the rotation
    would be around the origin instead of the new position. This algorithm
    places each block on the left and right of the origin so that the origin
    remains in the center of the hazard. This will allow for correct
    rotation

--- src/gameplay/hazard/hazard.gd
+++ src/gameplay/hazard/hazard.gd
@@ -1,9 +1,11 @@
 class_name Hazard
 extends Area2D
 
+const BLOCK_SIZE: float = 16
+
 enum HazardType { HORIZONTAL, VERTICAL, DIAGONAL_UP, DIAGONAL_DOWN }
 
-@export var length: float = 1
+@export var length: int = 1
 @export var type: HazardType = HazardType.HORIZONTAL
 @export var hazard_block_scene: PackedScene
 
@@ -20,57 +22,57 @@ func _ready() -> void:
 
 
 func _build_hazard() -> void:
-	var block_size: float
+	var is_even := length % 2 == 0
+	var starting_index := 0
 
-	for i in range(length):
-		var block := hazard_block_scene.instantiate() as Sprite2D
-		block_size = block.texture.get_size().x
+	if not is_even:
+		blocks.add_child(_create_block(0, 1))
+		starting_index = 2
 
-		var block_position := i * block_size
-		if type == HazardType.HORIZONTAL:
-			block.global_position.x = block_position
-		elif type == HazardType.VERTICAL:
-			block.global_position.y = block_position
-		elif type == HazardType.DIAGONAL_UP:
-			block.global_position.x = block_position
-			block.global_position.y = -block_position
-		elif type == HazardType.DIAGONAL_DOWN:
-			block.global_position.x = block_position
-			block.global_position.y = block_position
-
-		blocks.add_child(block)
+	var pair_num := 1
+	for i in range(starting_index, length, 2):
+		var offset := pair_num * BLOCK_SIZE
+		if is_even:
+			offset -= 8
+		blocks.add_child(_create_block(offset, 1))
+		blocks.add_child(_create_block(offset, -1))
+		pair_num += 1
 
 	var rectangle := RectangleShape2D.new()
-	var hazard_offset := (length - 1) * block_size / 2
-	var hazard_size := block_size * length
-	var diagonal_block_size := sqrt(pow(block_size, 2) * 2)
+	var hazard_size := BLOCK_SIZE * length
+	var diagonal_block_size := sqrt(pow(BLOCK_SIZE, 2) * 2)
 
 	if type == HazardType.HORIZONTAL:
-		global_position.x -= hazard_offset
-		collision_shape.position.x += hazard_offset
-		rectangle.size = Vector2(hazard_size, block_size)
+		rectangle.size = Vector2(hazard_size, BLOCK_SIZE)
 	elif type == HazardType.VERTICAL:
-		global_position.y -= hazard_offset
-		collision_shape.position.y += hazard_offset
-		rectangle.size = Vector2(block_size, hazard_size)
+		rectangle.size = Vector2(BLOCK_SIZE, hazard_size)
 	elif type == HazardType.DIAGONAL_UP:
-		global_position.x -= hazard_offset
-		global_position.y += hazard_offset
-		collision_shape.position.x += hazard_offset
-		collision_shape.position.y -= hazard_offset
 		collision_shape.rotation_degrees = -45
-		rectangle.size = Vector2(diagonal_block_size * length, block_size)
+		rectangle.size = Vector2(diagonal_block_size * length, BLOCK_SIZE)
 	elif type == HazardType.DIAGONAL_DOWN:
-		global_position.x -= hazard_offset
-		global_position.y -= hazard_offset
-		collision_shape.position.x += hazard_offset
-		collision_shape.position.y += hazard_offset
 		collision_shape.rotation_degrees = 45
-		rectangle.size = Vector2(diagonal_block_size * length, block_size)
+		rectangle.size = Vector2(diagonal_block_size * length, BLOCK_SIZE)
 
 	collision_shape.shape = rectangle
-
 	body_entered.connect(_on_body_entered)
+
+
+func _create_block(offset: float, side: int) -> Sprite2D:
+	var block := hazard_block_scene.instantiate() as Sprite2D
+
+	var block_position := offset * side
+	if type == HazardType.HORIZONTAL:
+		block.global_position.x = block_position
+	elif type == HazardType.VERTICAL:
+		block.global_position.y = block_position
+	elif type == HazardType.DIAGONAL_UP:
+		block.global_position.x = block_position
+		block.global_position.y = -block_position
+	elif type == HazardType.DIAGONAL_DOWN:
+		block.global_position.x = block_position
+		block.global_position.y = block_position
+
+	return block
 
 
 func _on_body_entered(_body: Node2D) -> void:
