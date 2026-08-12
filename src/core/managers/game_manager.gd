13fd8db2ab0ec28489bafaf7d4cde4423d82bc5d
is386 <singh.indervir89@gmail.com>
2026-08-11 22:29:01 -0400

    Add distance milestones

--- src/core/managers/game_manager.gd
+++ src/core/managers/game_manager.gd
@@ -2,6 +2,7 @@ class_name GameManager
 extends Node
 
 var distance: float = 0
+var milestone: float = 1000
 var _is_started: bool
 
 
@@ -13,6 +14,10 @@ func _physics_process(_delta: float) -> void:
 	if not _is_started:
 		return
 	distance += 0.2
+
+	if distance > milestone:
+		milestone += 1000
+		SignalBus.milestone_reached.emit()
 
 
 func _on_level_loaded() -> void:
