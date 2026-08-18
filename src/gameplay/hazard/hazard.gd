e0269bbe15a05f6c346214f4381580f40d5090d0
is386 <singh.indervir89@gmail.com>
2026-08-18 14:45:57 -0400

    Add wiggling effect to eels

--- src/gameplay/hazard/hazard.gd
+++ src/gameplay/hazard/hazard.gd
@@ -19,6 +19,9 @@ enum HazardType { HORIZONTAL, VERTICAL, DIAGONAL_UP, DIAGONAL_DOWN }
 @onready var despawn_timer: Timer = $DespawnTimer
 
 var block_size: float = 21
+var _blocks: Array[Sprite2D]
+var _current_block: int = 0
+var _time: float = 0
 
 
 func _ready() -> void:
@@ -40,6 +43,16 @@ func _physics_process(delta: float) -> void:
 		rotation -= delta
 
 	global_position.x -= speed * delta
+
+	_time += delta
+	if _time >= 0.15:
+		var next_block := _current_block + 1
+		if next_block == length:
+			next_block = 0
+		_blocks[next_block].offset.y = -1
+		_blocks[_current_block].offset.y = 0
+		_current_block = next_block
+		_time = 0
 
 
 func _build_hazard() -> void:
@@ -66,14 +79,28 @@ func _build_hazard() -> void:
 
 	if type == HazardType.VERTICAL:
 		rotation_degrees = 90
+		_blocks.sort_custom(_sort_blocks_y)
+	elif type == HazardType.HORIZONTAL:
+		_blocks.sort_custom(_sort_blocks_x)
 	elif type == HazardType.DIAGONAL_UP:
 		rotation_degrees = -45
+		_blocks.sort_custom(_sort_blocks_x)
 	elif type == HazardType.DIAGONAL_DOWN:
 		rotation_degrees = 45
+		_blocks.sort_custom(_sort_blocks_x)
+
+
+func _sort_blocks_x(a: Sprite2D, b: Sprite2D) -> bool:
+	return a.global_position.x <= b.global_position.x
+
+
+func _sort_blocks_y(a: Sprite2D, b: Sprite2D) -> bool:
+	return a.global_position.y <= b.global_position.y
 
 
 func _create_block(offset: float, side: int, is_head_or_tail: bool) -> Sprite2D:
 	var block := Sprite2D.new()
+	_blocks.append(block)
 
 	if is_head_or_tail:
 		if side == -1:
