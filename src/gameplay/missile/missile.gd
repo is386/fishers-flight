3262e3f95eb02d9b14f66b9e6eb9d237e7cf4f3b
is386 <20195347+is386@users.noreply.github.com>
2026-08-19 19:58:30 -0400

    Add bubble trail effect to missile

--- src/gameplay/missile/missile.gd
+++ src/gameplay/missile/missile.gd
@@ -4,11 +4,13 @@ extends Area2D
 @export var speed: float = 200
 
 @onready var visibility_notifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var particles: CPUParticles2D = $CPUParticles2D
 
 
 func _ready() -> void:
 	visibility_notifier.screen_exited.connect(_on_screen_exited)
 	body_entered.connect(_on_body_entered)
+	particles.finished.connect(_on_particles_finished)
 
 
 func _physics_process(delta: float) -> void:
@@ -21,4 +23,8 @@ func _on_body_entered(_body: Node2D) -> void:
 
 
 func _on_screen_exited() -> void:
+	particles.emitting = false
+
+
+func _on_particles_finished() -> void:
 	queue_free()
